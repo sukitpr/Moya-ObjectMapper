a759aa1b9d58521e8276b9bb52a0ddd371edb11c
Sukit Praparaweewan <sukit.p@linecorp.com>
2019-03-26 15:46:54 +0700

    Added project files

---
+++ Moya-ObjectMapper/Moya_ObjectMapper.h
@@ -0,0 +1,18 @@
+//
+//  Moya_ObjectMapper.h
+//  Moya-ObjectMapper
+//
+//  Created by Sukit on 26/3/2562 BE.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Moya_ObjectMapper.
+FOUNDATION_EXPORT double Moya_ObjectMapperVersionNumber;
+
+//! Project version string for Moya_ObjectMapper.
+FOUNDATION_EXPORT const unsigned char Moya_ObjectMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moya_ObjectMapper/PublicHeader.h>
+
+
